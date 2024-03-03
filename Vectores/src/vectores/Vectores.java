7a55c9542dfede87a03b586428e1992fbbbdc26f
JohannDav <quirino.gonzalez.johann.david@gmail.com>
2024-03-02 19:53:30 -0600

    Vectores

---
+++ Vectores/src/vectores/Vectores.java
@@ -0,0 +1,17 @@
+package vectores;
+public class Vectores {
+    public static void main(String[] args) {
+        
+        //Declaracion
+        int vector [] = new int [4];
+        
+        //Asignacion (manual)
+        
+        vector[0] = 2;
+        vector[1] = 35;
+        vector[2] = 48;
+        vector[3] = 157;
+        
+        
+    }
+}
