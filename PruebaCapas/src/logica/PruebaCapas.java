954f784e0ae8bfed13b09f444ca5d6600c4b6209
JohannDav <quirino.gonzalez.johann.david@gmail.com>
2024-03-03 00:40:06 -0600

    Edicion capas (Salto de videos)

---
+++ PruebaCapas/src/logica/PruebaCapas.java
@@ -0,0 +1,16 @@
+package logica;
+
+public class PruebaCapas {
+
+    public static void main(String[] args) {
+        
+        /* 
+        Este Proyecto solo tiene como finalidad demostrar como se deben ver
+        las capas de los proyectos en NetBeans. Modelo de capas: IGU, Logica
+        y Persistencia de TODOCODE.
+        
+        */
+        
+    }
+    
+}
