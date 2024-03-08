10a0ced0707e5338a01ca03568857cde72bbf791
JohannDav <quirino.gonzalez.johann.david@gmail.com>
2024-03-08 02:00:57 -0600

    Lista con asignacion por teclado, manual y recorrido

--- Vectores/src/vectores/Vectores.java
+++ Vectores/src/vectores/Vectores.java
@@ -1,27 +1,33 @@
 package vectores;
+
+import java.util.Scanner;
+
 public class Vectores {
     public static void main(String[] args) {
-        
         //Declaracion
         int vector [] = new int [4];
         
-        //Asignacion (manual)
-        
-        vector[0] = 2;
-        vector[1] = 35;
-        vector[2] = 48;
-        vector[3] = 157;
-        
-        //Recorrido
-        
-        for (int i = 0; i < vector.length; i ++){
-            System.out.println("Estoy en el indice " + i);
-            System.out.println("Tengo guardado un: " + vector [i]);
-            System.out.println("----------------------------");
-            
-            /* EL for da esos valores debido a q i esta relacionado con
-            el valor del indice "Los []", por ello se puede usar el [i]*/
-        }
-        
+        //Asignacion por teclado
+        Scanner teclado = new Scanner (System.in);
+       for(int i = 0; i < vector.length; i ++){
+           System.out.println("Ingrese el valor para el índice " + i);
+           vector [i] = teclado.nextInt();           
+       }
+       
+       /*Asignacion manual
+       vector [0] = 2;
+       vector [1] = 35;
+       vector [2] = 48;
+       vector [3] = 157; */
+       
+       //Recorrido
+       for(int i = 0; i < vector.length; i ++ ){
+           System.out.println("Estoy en el indice: " + i );
+           System.out.println("Tengo un: " + vector[i]);
+           System.out.println("----------------------");
+       }
+       
+       
     }
+    
 }
