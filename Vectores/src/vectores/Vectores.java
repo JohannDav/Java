f0d04841c6e3c05ee259fef19e06487930bde4b2
JohannDav <quirino.gonzalez.johann.david@gmail.com>
2024-03-08 22:23:38 -0600

    Agregar Matrices con asignacion manual, teclado y recorrido

--- Vectores/src/vectores/Vectores.java
+++ Vectores/src/vectores/Vectores.java
@@ -7,27 +7,28 @@ public class Vectores {
         //Declaracion
         int vector [] = new int [4];
         
+        /*Asignacion manual
+        vector [0] = 2;
+        vector [1] = 35;
+        vector [2] = 48;
+        vector [3] = 157;*/
+        
         //Asignacion por teclado
-        Scanner teclado = new Scanner (System.in);
-       for(int i = 0; i < vector.length; i ++){
-           System.out.println("Ingrese el valor para el índice " + i);
-           vector [i] = teclado.nextInt();           
-       }
-       
-       /*Asignacion manual
-       vector [0] = 2;
-       vector [1] = 35;
-       vector [2] = 48;
-       vector [3] = 157; */
-       
-       //Recorrido
-       for(int i = 0; i < vector.length; i ++ ){
-           System.out.println("Estoy en el indice: " + i );
-           System.out.println("Tengo un: " + vector[i]);
-           System.out.println("----------------------");
-       }
-       
-       
+        Scanner teclado = new Scanner(System.in);
+        
+        for (int i = 0; i < vector.length; i ++){
+            System.out.println("Ingrese el valor para el indice " + i);
+            vector [i] = teclado.nextInt();
+        }
+        
+        //Recorrido
+        
+        for(int i = 0; i < vector.length; i ++){
+            System.out.println("------------------");
+            System.out.println("Estoy en el indice: " + i);
+            System.out.println("Y tengo un: " + vector[i]);
+        }
+        
     }
     
 }
